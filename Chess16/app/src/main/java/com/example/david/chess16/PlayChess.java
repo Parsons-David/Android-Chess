bba4a0c7bb45ea7490100723e280067ca232a612
Parsons-David <david.parsons@rutgers.edu>
2017-04-22 22:05:57 -0500

    Started Play Activity

---
+++ Chess16/app/src/main/java/com/example/david/chess16/PlayChess.java
@@ -0,0 +1,13 @@
+package com.example.david.chess16;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayChess extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_chess);
+    }
+}
